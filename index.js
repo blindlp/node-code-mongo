2fddec6490253f939a68794f7b27578071ecfa1a
blindlp <luispoz@gmail.com>
2020-07-12 01:26:07 -0600

    moongose connection

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
+require("dotenv").config();
 
+const mongoose = require("mongoose");
+
+const Student = mongoose.model("Students", {
+  name: {
+    type: String,
+    required: true,
+  },
+  course: {
+    type: String,
+    required: true,
+    enum: ["Programacion", "Ingles", "Cocina"],
+  },
+  age: {
+    type: Number,
+    min: 1,
+  },
+});
+
+mongoose.connect(
+  process.env.DATEBASE,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => {
+    console.log("DB connected");
+
+    Student.create({
+      name: "Luis Pozuelos",
+      course: "Programacion",
+      age: 40,
+    });
+  }
+);
